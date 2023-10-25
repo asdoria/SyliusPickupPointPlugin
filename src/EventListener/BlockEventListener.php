cb4e802f1d5f10d2966b85af0b3fa314bd044f8d
philippe vesin <philippe.vesin@asdoria.com>
2023-10-25 09:38:48 +0200

    [fix] override block listener

---
+++ src/EventListener/BlockEventListener.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Asdoria\SyliusPickupPointPlugin\EventListener;
+
+use Sylius\Bundle\UiBundle\Block\BlockEventListener as BaseBlockEventListener;
+use Sonata\BlockBundle\Model\Block;
+
+/**
+ * Class BlockEventListener
+ * @package Asdoria\SyliusPickupPointPlugin\EventListener
+ *
+ * @author  Philippe Vesin <pve.asdoria@gmail.com>
+ */
+final class BlockEventListener
+{
+
+    public function onBlockEvent(BlockEvent $event): void
+    {
+
+    }
+}
